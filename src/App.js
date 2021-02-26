86933c780e96d7ea75bc1db47ac0a8d181bbc022
manuelvilla <manuelvillasenor.sa@gmail.com>
2021-02-26 13:51:07 -0600

    =Estilos de forminput y custombutton para sign in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInUp from './pages/signin-up/signin-up.component';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-up/signin-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './signin-up.styles.scss';
+
+import SignIn from '../../components/signin/signin.component';
+
+const SignInUp = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+);
+
+export default SignInUp;
